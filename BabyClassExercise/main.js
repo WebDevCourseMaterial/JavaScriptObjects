c3d099a7b7b64ac9523da332f664e2b594722a1f
Dave Fisher <fisherds@rose-hulman.edu>
2014-10-28 16:07:53 -0400

    Indent changes

--- BabyClassExercise/main.js
+++ BabyClassExercise/main.js
@@ -2,21 +2,23 @@
 var rh = rh || {};
 rh.objectbasics = rh.objectbasics || {};
 
-$(document).ready( function() {
+$(document).ready(function() {
   var mckinley = new rh.objectbasics.Baby('McKinley');
   var keegan = new rh.objectbasics.Baby('Keegan');
   var bowen = new rh.objectbasics.Baby('Bowen');
-  
-  for (var i = 0; i < 2; ++i) {
-	keegan.hourPasses();
-	bowen.hourPasses();
-	bowen.feedBaby();
-	mckinley.feedBaby();
-	for (var j = 0; j < 4; ++j) {
-	  mckinley.hourPasses();
-	}
+
+  for ( var i = 0; i < 2; ++i) {
+    keegan.hourPasses();
+    bowen.hourPasses();
+    bowen.feedBaby();
+    mckinley.feedBaby();
+    for ( var j = 0; j < 4; ++j) {
+      mckinley.hourPasses();
+    }
   }
   keegan.hourPasses();
   bowen.hourPasses();
-  
+
 });
+
+

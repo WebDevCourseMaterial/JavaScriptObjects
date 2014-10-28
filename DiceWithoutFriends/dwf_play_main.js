eac43332b284e134ff9a35a717ee55a4e01022cd
Dave Fisher <fisherds@rose-hulman.edu>
2014-10-27 22:27:01 -0400

    Adding a starting point

---
+++ DiceWithoutFriends/dwf_play_main.js
@@ -0,0 +1,11 @@
+/* Namespace */
+var rh = rh || {};
+rh.dwf = rh.dwf || {};
+
+$(document).ready( function() {
+
+  // TODO: Create an instance of the rh.dwf.DiceGameController.
+  
+  // TODO: Simply reload the page if there is a new game press.
+  
+});
